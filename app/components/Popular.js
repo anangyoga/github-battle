a5109d4807c1f34a7831d68ddb1864a7a00883b8
anangyoga <aharwinto@gmail.com>
2022-03-04 21:46:11 +0700

    feat: add state to navbar

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,21 @@
 import React from "react";
 
 class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: "All",
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage,
+    });
+  }
   render() {
     const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Phyton"];
 
@@ -8,7 +23,9 @@ class Popular extends React.Component {
       <ul className="flex-center">
         {languages.map((language) => (
           <li key={language}>
-            <button className="btn-clear nav-link">{language}</button>
+            <button style={language === this.state.selectedLanguage ? { color: "red" } : null} className="btn-clear nav-link" onClick={() => this.updateLanguage(language)}>
+              {language}
+            </button>
           </li>
         ))}
       </ul>
